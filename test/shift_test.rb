46ffaf663ac6b2880aaf2c9a2834a6b7ab033e72
abenetka <ali.benetka@gmail.com>
2018-11-03 16:17:50 -0600

    Updates test for total offset method

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -21,14 +21,14 @@ class ShiftTest < Minitest::Test
     assert_equal 6, shift.date_generator.length
   end
 
-  def test_it_can_calculate_key_shift
-    shift = Shift.new
-    key =  "01234"
-    assert_equal "01", shift.key_shift_a(key)
-    assert_equal "12", shift.key_shift_b(key)
-    assert_equal "23", shift.key_shift_c(key)
-    assert_equal "34", shift.key_shift_d(key)
-  end
+  # def test_it_can_calculate_key_shift
+  #   shift = Shift.new
+  #   key =  "01234"
+  #   assert_equal "01", shift.key_shift_a(key)
+  #   assert_equal "12", shift.key_shift_b(key)
+  #   assert_equal "23", shift.key_shift_c(key)
+  #   assert_equal "34", shift.key_shift_d(key)
+  # end
 
   def test_it_can_calculate_date_offset
     shift = Shift.new
@@ -40,10 +40,10 @@ class ShiftTest < Minitest::Test
     shift = Shift.new
     key = "01234"
     date = "220489"
-    assert_equal "10", shift.total_shift_a
-    assert_equal "11", shift.total_shift_b
-    assert_equal "25", shift.total_shift_d
-    assert_equal "25", shift.total_shift_d
+    assert_equal 10, shift.total_shift_a(key, date)
+    assert_equal 13, shift.total_shift_b(key, date)
+    assert_equal 25, shift.total_shift_c(key, date)
+    assert_equal 35, shift.total_shift_d(key, date)
   end
 
 
