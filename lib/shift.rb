4d34a860b99fa59d404de36ab3c457d0b5e739cd
abenetka <ali.benetka@gmail.com>
2018-11-03 15:49:50 -0600

    Adds implementation of key shift and date offset methods

--- lib/shift.rb
+++ lib/shift.rb
@@ -6,12 +6,44 @@ class Shift
   end
 
   def key_generator
-    ('0'..'99999').to_a.sample.rjust(2, '0')
+    ('0'..'99999').to_a.sample.rjust(5, '0')
   end
 
   def date_generator
     Date.today.strftime('%d%m%y')
   end
+
+  def date_offset(date)
+    squared = date.to_i ** 2
+    squared.to_s.split(//).last(4).join
+  end
+
+
+  def key_shift_a(key)
+    split = key.chars
+    split[0] + split[1]
+  end
+
+  def key_shift_b(key)
+    split = key.chars
+    split[1] + split[2]
+  end
+
+  def key_shift_c(key)
+    split = key.chars
+    split[2] + split[3]
+  end
+
+  def key_shift_d(key)
+    split = key.chars
+    split[3] + split[4]
+  end
+
+
+
+
+
+
 
 
 
