1c550a4da70fb7e74ef3659c3a0169995c06bcb8
abenetka <ali.benetka@gmail.com>
2018-11-03 15:49:26 -0600

    Adds tests for calculating key shift and date offset

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -21,6 +21,23 @@ class ShiftTest < Minitest::Test
     assert_equal 6, shift.date_generator.length
   end
 
+  def test_it_can_calculate_key_shift
+    shift = Shift.new
+    key =  "01234"
+    assert_equal "01", shift.key_shift_a(key)
+    assert_equal "12", shift.key_shift_b(key)
+    assert_equal "23", shift.key_shift_c(key)
+    assert_equal "34", shift.key_shift_d(key)
+  end
+
+  def test_it_can_calculate_date_offset
+    shift = Shift.new
+    date = "220489"
+    assert_equal "9121", shift.date_offset(date)
+  end
+
+  
+
 
 
 
