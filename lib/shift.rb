23544c92d61fcfd48a3dbbb95b3f18a550443aa7
abenetka <ali.benetka@gmail.com>
2018-11-04 11:50:51 -0700

    Updates total shift method and initialize

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,31 +1,30 @@
 class Shift
   attr_reader :key, :date
-  def initialize
+  def initialize(key, date)
     @key = key
     @date = date
   end
 
-
-
-  def date_offset(date)
+  def date_offset
     offset = date.to_i ** 2
     offset.to_s.split(//).last(4).join
   end
 
-  def total_shift_a(key, date)
-    key[0..1].to_i + date_offset(date)[0].to_i
+  def total_shift_a
+    # binding.pry
+    key[0..1].to_i + date_offset[0].to_i
   end
 
-  def total_shift_b(key, date)
-    key[1..2].to_i + date_offset(date)[1].to_i
+  def total_shift_b
+    key[1..2].to_i + date_offset[1].to_i
   end
 
-  def total_shift_c(key, date)
-    key[2..3].to_i + date_offset(date)[2].to_i
+  def total_shift_c
+    key[2..3].to_i + date_offset[2].to_i
   end
 
-  def total_shift_d(key, date)
-    key[3..4].to_i + date_offset(date)[3].to_i
+  def total_shift_d
+    key[3..4].to_i + date_offset[3].to_i
   end
 
     #
