89b942e746452dff42fb651698771acfcabd242b
abenetka <ali.benetka@gmail.com>
2018-11-04 11:50:19 -0700

    Updates total shift method test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -7,7 +7,7 @@ require 'pry'
 
 class ShiftTest < Minitest::Test
   def test_it_exists
-    shift = Shift.new
+    shift =  Shift.new("01234", "220489")
     assert_instance_of Shift, shift
   end
 
@@ -23,19 +23,18 @@ class ShiftTest < Minitest::Test
   # end
 
   def test_it_can_calculate_date_offset
-    shift = Shift.new
-    date = "220489"
-    assert_equal "9121", shift.date_offset(date)
+    shift = Shift.new("01234", "220489")
+
+    assert_equal "9121", shift.date_offset
   end
 
   def test_it_calculate_total_shift
-    shift = Shift.new
-    key = "01234"
-    date = "220489"
-    assert_equal 10, shift.total_shift_a(key, date)
-    assert_equal 13, shift.total_shift_b(key, date)
-    assert_equal 25, shift.total_shift_c(key, date)
-    assert_equal 35, shift.total_shift_d(key, date)
+    shift = Shift.new("01234", "220489")
+
+    assert_equal 10, shift.total_shift_a
+    assert_equal 13, shift.total_shift_b
+    assert_equal 25, shift.total_shift_c
+    assert_equal 35, shift.total_shift_d
   end
 
 end
