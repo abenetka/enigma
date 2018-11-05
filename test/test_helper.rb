04a2cd739c49dfdb833fcecc83a5fb9152ecc8de
abenetka <ali.benetka@gmail.com>
2018-11-05 13:42:30 -0700

    Updates test files to include test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,9 @@
 require 'simplecov'
 SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require './lib/shift'
+require './lib/encryption'
+require 'date'
+require 'pry'
