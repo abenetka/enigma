0cbe05253d699284d3751e86fb58222cd0f66516
abenetka <ali.benetka@gmail.com>
2018-11-05 11:22:32 -0700

    Adds more assertions in test for encryption and updates split message test

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -10,23 +10,42 @@ class EncryptionTest < Minitest::Test
      assert_instance_of Encryption, encryption
   end
 
-  def test_it_can_break_down_a_message_into_small_arrays
+  def test_it_can_split_a_message
     encryption = Encryption.new("hello world", "01234", "220489")
-
-    expected = [["h", "e", "l", "l"], ["o", " ", "w", "o"], ["r", "l", "d"]]
+    expected = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
     assert_equal expected, encryption.split_message
   end
 
-  def test_it_can_find_an_index
-    encryption = Encryption.new("ali", "01234", "220489")
-    expected = "kyg"
-    assert_equal expected, encryption.encryption
+  def test_it_can_encrypt_a_message
+    encryption_1 = Encryption.new("ali", "01234", "220489")
+    encryption_2 = Encryption.new("hello", "01234", "220489")
+    encryption_3 = Encryption.new("encryption is fun", "01234", "220489")
+    expected_1 = "kyg"
+    expected_2 = "rrjty"
+    expected_3 = "o azhbrqy yqbmdbx"
+
+    assert_equal expected_1, encryption_1.encryption
+    assert_equal 3, encryption_1.encryption.length
+    assert_equal expected_2, encryption_2.encryption
+    assert_equal 5, encryption_2.encryption.length
+    assert_equal expected_3, encryption_3.encryption
+    assert_equal 17, encryption_3.encryption.length
+
   end
 
   def test_it_can_assign_shift_based_on_index
     encryption = Encryption.new("hello world", "01234", "220489")
-    index = 1
-    assert_equal  13 , encryption.shifter(index)
+    index_1 = 1
+    index_2 = 10
+    index_3 = 3
+    index_4 = 5
+    index_5 = 8
+
+    assert_equal  13 , encryption.shifter(index_1)
+    assert_equal  25 , encryption.shifter(index_2)
+    assert_equal  35 , encryption.shifter(index_3)
+    assert_equal  13, encryption.shifter(index_4)
+    assert_equal  10 , encryption.shifter(index_5)
   end
 
 
