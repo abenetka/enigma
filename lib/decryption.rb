3e4fcf585a51e9c340481d4178e5754382d60d65
abenetka <ali.benetka@gmail.com>
2018-11-06 16:54:51 -0700

    Adds character set into initialize and adjusts for change throughout class

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -1,15 +1,12 @@
 class Decryption
-  attr_reader :message, :key, :date, :shift
+  attr_reader :message, :key, :date, :shift, :character_set
 
   def initialize(message, key, date)
     @message = message
     @key = key
     @date = date
     @shift = Shift.new(key, date)
-  end
-
-  def char_set
-    ("a".."z").to_a << " "
+    @character_set = ("a".."z").to_a << " "
   end
 
   def split_message
@@ -18,11 +15,11 @@ class Decryption
 
   def decryption
     new_message = split_message.each_with_index.map do |character, index|
-      char_index = char_set.index(character)
+      char_index = @character_set.index(character)
       if char_index
         rotation = @shift.shifter(index)
         total_rot = char_index - rotation
-        char_set.rotate(total_rot).first
+        @character_set.rotate(total_rot).first
       else
         character
       end
@@ -30,6 +27,21 @@ class Decryption
     new_message.join
   end
 
+  def encrypt
+    split_message.each_with_index.map do |character, index|
+      rotation(character, index)
+    end.join
+  end
+
+  def rotation(character, index)
+    character_index = @character_set.index(character)
+    if character_index
+      total_rotation = character_index + @shift.shifter(index)
+      @character_set.rotate(total_rotation).first
+    else
+      character
+    end
+  end
 
 
 
