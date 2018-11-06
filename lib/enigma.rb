21912c1ddbc0d285b0b7d766316bba10e24da5cb
abenetka <ali.benetka@gmail.com>
2018-11-06 15:50:19 -0700

    Refactors encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -23,21 +23,19 @@ class Enigma
   def encrypt(message, key = key_generator, date = date_generator)
     encryption_hash = Hash.new(0)
     message = Encryption.new(message, key, date)
-    encrypted_message = message.encryption
-    encryption_hash[:encryption] = encrypted_message
+    encryption_hash[:encryption] = message.encryption
     encryption_hash[:key] = key
     encryption_hash[:date] = date
     encryption_hash
   end
 
   def decrypt(message, key = key_generator, date = date_generator)
-    decrypted_hash = Hash.new(0)
+    decryption_hash = Hash.new(0)
     message = Decryption.new(message, key, date)
-    decrypted_message = message.decryption
-    decrypted_hash[:decryption] = decrypted_message
-    decrypted_hash[:key] = key
-    decrypted_hash[:date] = date
-    decrypted_hash
+    decryption_hash[:decryption] = message.decryption
+    decryption_hash[:key] = key
+    decryption_hash[:date] = date
+    decryption_hash
   end
 
 
