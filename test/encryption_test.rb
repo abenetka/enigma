d6ce48b3c8264467b68160bbadd2eed990640004
abenetka <ali.benetka@gmail.com>
2018-11-06 16:50:21 -0700

    Refactors syntax in tests

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -14,14 +14,14 @@ class EncryptionTest < Minitest::Test
     assert_equal expected, encryption.split_message
   end
 
-    def test_it_can_create_a_character_set
-      encryption = Encryption.new("hello world", "01234", "220489")
+  def test_it_can_create_a_character_set
+    encryption = Encryption.new("hello world", "01234", "220489")
 
-      expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
-      assert_equal expected, encryption.character_set
-    end
+    assert_equal expected, encryption.character_set
+  end
 
   def test_it_can_encrypt_a_message
     encryption_1 = Encryption.new("ali", "01234", "220489")
@@ -31,28 +31,26 @@ class EncryptionTest < Minitest::Test
     expected_2 = "rrjty"
     expected_3 = "o azhbrqy yqbmdbx"
 
-    assert_equal expected_1, encryption_1.encryption
-    assert_equal 3, encryption_1.encryption.length
-    assert_equal expected_2, encryption_2.encryption
-    assert_equal 5, encryption_2.encryption.length
-    assert_equal expected_3, encryption_3.encryption
-    assert_equal 17, encryption_3.encryption.length
+    assert_equal expected_1, encryption_1.encrypt
+    assert_equal expected_2, encryption_2.encrypt
+    assert_equal expected_3, encryption_3.encrypt
+    assert_equal 3, encryption_1.encrypt.length
+    assert_equgal 5, encryption_2.encrypt.length
+    assert_equal 17, encryption_3.encrypt.length
   end
 
   def test_it_can_encrypt_a_message_with_characters_not_in_character_set
     encryption = Encryption.new("hello world!", "02715", "040895")
 
     expected = "keder ohulw!"
-    assert_equal expected, encryption.encryption
+    assert_equal expected, encryption.encrypt
   end
 
   def test_it_can_encrypt_a_message_with_uppercase_characters
     encryption = Encryption.new("Hello World", "02715", "040895")
 
     expected = "keder ohulw"
-    assert_equal expected, encryption.encryption
+    assert_equal expected, encryption.encrypt
   end
-
-
 
 end
