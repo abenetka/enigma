7ba39ce16822d1d319dfe01d6d3a0dac05d9be5e
abenetka <ali.benetka@gmail.com>
2018-11-06 16:43:57 -0700

    Refactors encryption method and creates rotation helper method

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -16,18 +16,19 @@ class Encryption
   end
 
   def encryption
-    new_message = split_message.each_with_index.map do |character, index|
-      # binding.pry
-      char_index = @character_set.index(character)
-      if char_index
-        rotation = @shift.shifter(index)
-        total_rot = char_index + rotation
-        @character_set.rotate(total_rot).first
-      else
-        character
-      end
-    end
-    new_message.join
+    split_message.each_with_index.map do |character, index|
+      rotation(character, index)
+    end.join
   end
 
+  def rotation(character, index)
+    character_index = @character_set.index(character)
+    if character_index
+      total_rotation = character_index + @shift.shifter(index)
+      @character_set.rotate(total_rotation).first
+    else
+      character
+    end
+  end
+g
 end
