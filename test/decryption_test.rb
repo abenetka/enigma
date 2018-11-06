44293d3730921e0e44862a28fc809bb1df84e544
abenetka <ali.benetka@gmail.com>
2018-11-06 16:53:51 -0700

    Adds test for character set initialization in decryption class

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -6,6 +6,15 @@ class DecryptionTest < Minitest::Test
     assert_instance_of Decryption, decryption
   end
 
+  def test_it_can_create_a_character_set
+    decryption = Decryption.new("hello world", "01234", "220489")
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, decryption.character_set
+  end
+
   def test_it_can_decrypt_a_message
     decryption = Decryption.new("keder ohulw", "02715", "040895")
 
