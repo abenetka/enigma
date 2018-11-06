527f53a590b453504d3022871f32162a9bac9061
abenetka <ali.benetka@gmail.com>
2018-11-06 14:33:40 -0700

    Adds test for decryption of characters that are not in character set

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -13,6 +13,14 @@ class DecryptionTest < Minitest::Test
     assert_equal expected, decryption.decryption
   end
 
+  def test_it_can_decrypt_a_message_with_characters_not_in_character_set
+    decryption = Decryption.new("keder ohulw!", "02715", "040895")
+
+    expected = "hello world!"
+    assert_equal expected, decryption.decryption
+  end
+
+
 
 
 end
